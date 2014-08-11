55a2c3e78fb3bd7eedac4e36a13334e1fb77fd3d
RyoAbe <ryoabe.gm@gmail.com>
2014-08-11 22:10:26 +0900

    initial commit

---
+++ 01_App/PARTNER/PARTNER/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PARTNER
+//
+//  Created by RyoAbe on 2014/08/10.
+//  Copyright (c) 2014年 RyoAbe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
